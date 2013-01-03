3947199881e30e0e84c58ab056a495143213d42d
Mark Price <markalanprice@gmail.com>
2013-01-03 09:08:23 -0700

    Removed install's database autoload

--- install/config/autoload.php
+++ install/config/autoload.php
@@ -52,7 +52,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array('database', 'form_validation');
+$autoload['libraries'] = array('form_validation');
 
 
 /*
