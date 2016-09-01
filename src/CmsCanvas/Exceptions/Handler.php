9149280b915b982537c6b06f1044e3305e9871b8
diyphpdeveloper <markalanprice@gmail.com>
2016-09-01 01:58:07 +0000

    Removed boot method from route service provider

--- src/CmsCanvas/Exceptions/Handler.php
+++ src/CmsCanvas/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace CmsCanvas\Exceptions;
 
 use Exception, App, Response;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 
@@ -57,6 +58,22 @@ class Handler extends ExceptionHandler {
         }
 
         return parent::render($request, $e);
+    }
+
+    /**
+     * Convert an authentication exception into an unauthenticated response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Auth\AuthenticationException  $exception
+     * @return \Illuminate\Http\Response
+     */
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        if ($request->expectsJson()) {
+            return response()->json(['error' => 'Unauthenticated.'], 401);
+        }
+
+        return redirect()->guest('login');
     }
 
 }
--- src/CmsCanvas/Providers/RouteServiceProvider.php
+++ src/CmsCanvas/Providers/RouteServiceProvider.php
@@ -42,19 +42,6 @@ class RouteServiceProvider extends ServiceProvider
     protected $contentTypes;
 
     /**
-     * Define your route model bindings, pattern filters, etc.
-     *
-     * @param  \Illuminate\Routing\Router  $router
-     * @return void
-     */
-    public function boot(Router $router)
-    {
-        //
-
-        parent::boot($router);
-    }
-
-    /**
      * Define the routes for the application.
      *
      * @param  \Illuminate\Routing\Router  $router
