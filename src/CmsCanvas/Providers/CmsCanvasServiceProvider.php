b22e5106b7f8f1e75d66f539ff2e33c1890d1024
diyphpdeveloper <markalanprice@gmail.com>
2016-12-17 02:29:38 +0000

    Updated version number to v2.1.3

--- src/CmsCanvas/Providers/CmsCanvasServiceProvider.php
+++ src/CmsCanvas/Providers/CmsCanvasServiceProvider.php
@@ -13,7 +13,7 @@ use CmsCanvas\Models\Setting;
 
 class CmsCanvasServiceProvider extends ServiceProvider {
 
-    const VERSION = '2.1.2';
+    const VERSION = '2.1.3';
 
     /**
      * Indicates if loading of the provider is deferred.
