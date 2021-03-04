38c749e5f6eb83605aa36364e62502dceadfcb36
diyphpdeveloper <markalanprice@gmail.com>
2021-03-04 02:42:48 +0000

    Updated breadcrumbs in admin theme

--- src/resources/themes/admin/views/partials/breadcrumbs.blade.php
+++ src/resources/themes/admin/views/partials/breadcrumbs.blade.php
@@ -1,16 +1,16 @@
 @if (!empty($breadcrumbs))
     <ul>
         @if (in_array(Route::currentRouteName(), array('admin.index', 'admin.dashboard')))
-            <li><span class="first_crumb current">Dashboard</span></li>
+            <li class="breadcrumb-item"><span class="active">Dashboard</span></li>
         @else
-            <li><a class="first_crumb" href="{{ route('admin.index') }}">Dashboard</a></li>
+            <li class="breadcrumb-item"><a href="{{ route('admin.index') }}">Dashboard</a></li>
         @endif
 
         @foreach($breadcrumbs as $uri => $crumb)
             @if (Request::is($uri))
-                <li><span class="current">{{ $crumb }}</span></li>
+                <li class="breadcrumb-item"><span class="current">{{ $crumb }}</span></li>
             @else
-                <li><a href="{{ Admin::url($uri) }}">{{ $crumb }}</a></li>
+                <li class="breadcrumb-item"><a href="{{ Admin::url($uri) }}">{{ $crumb }}</a></li>
             @endif
         @endforeach
     </ul>
